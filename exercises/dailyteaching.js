ed28cde739ec66f0c49861c5f5cca10430ad83a1
Waylon Gaye <waylongaye@gmail.com>
2019-01-29 10:18:02 -0700

    grid, dailyteaching

---
+++ exercises/dailyteaching.js
@@ -0,0 +1,56 @@
+// Conditionals
+
+    // Booleans
+        // true or false statements (only two values)
+        // var iLoveTree = true
+        // console.log(true)
+    
+    // if/else if/else Statements
+        // var bagOfFlowers = "oz"
+        // // var bagOfGreen = "gram"
+        //     if(bagOfFlowers == "oz"){
+        //         console.log("sounds like a deal")
+        //     } else {
+        //         console.log("okay, we have problem.")
+        //     }
+
+        // console.log( 3.5 >= 3.5)
+
+
+    // Comparison operators
+        // == checks type, but use coercion to the same types (2 = "2")
+        // === checks the type (does not use coercion)
+        // "==" "===" "> , < , <=, =>, !="
+
+
+    // Using Operators
+        // var reggieBush = 1
+        // var premiumFlower = 10
+        // if(reggieBush < premiumFlower){
+        //     console.log("that makes sense")
+        // } else {
+        //     console.log("that doesn't make sense")
+        // }
+
+    // The terms "truthy" and "falsey" refer to values that the computer can infer true or false from, even if they aren't booleans. 
+    
+
+
+
+
+    // var bagOfFlowers = "a gram"
+        //     var bagOfGreen = " eighth"
+        //         if(bagOfFlowers == "half ounce"){
+        //             console.log("sounds like a deal")
+        //         } else if (bagOfGreen == "half ounce"){
+        //             console.log("okay, we have problem.")
+        //         } else {
+        //             console.log("you got robbed!")
+        //         }
+    
+    
+    
+    // Ternary Operators
+        // //whatFlower = "girl scout cookies"
+        // goodMood = (whatFlower === "girl scout cookies") ? "happy" : goodMood
+        // console.log(goodMood)
