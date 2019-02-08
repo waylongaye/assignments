4e1c128465c9c7babae02b7c78daaf797f97b52f
Waylon Gaye <waylongaye@gmail.com>
2019-02-08 15:16:12 -0700

    exercises & notes

--- notes/js-notes/nested-loops.js
+++ notes/js-notes/nested-loops.js
@@ -1,13 +1,13 @@
-// var multiDimensionalArr = [[1,2,3],[4,22,5,6],[7,8,9]]
+// var matrix = [[1,2,3],[5,6,7],[9,8,4]]
 
-// for(var i = 0; i < multiDimensionalArr.length;i++){
+// for(var i = 0; i < matrix.length;i++){
 //     // console.log(multiDimensionalArr[i])
-//     for(var j = 0; j < multiDimensionalArr[i].length;j++ ){
-//       console.log(multiDimensionalArr[i][j])
+//     for(var j = 0; j < matrix[i].length;j++ ){
+//       console.log(matrix[i][j])
 //     }
 // }
 
-// console.log(multiDimensionalArr[1][1])
+// console.log(matrix[1][1])
 
 
 // [[0,0,0],
@@ -15,12 +15,22 @@
 //  [0,0,0]]
 var matrix = []
 
-for(var i = 2; i < 3;i++){
+for(var i = 0; i < 3; i++){
   matrix.push([])
-  for(var a = 0; a < 3;a++){
+  for(var a = 0; a < 3; a++){
     matrix[i].push(0)
   }
 }
 console.log(matrix)
 
 // matrix = [[0,0,0],[0,0,0],[0,0,0]]
+
+
+// function Person (name, isAlive, hairColor){
+//   this.name = name;
+//   this.isAlive = isAlive;
+//   this.hairColor - hairColor;
+//   this.speak = function speak(){
+//     console.log(name)
+//   }
+// }
