9108ea8a41da29cedc8c225fc3648c8c11ed4bb9
Waylon Gaye <waylongaye@gmail.com>
2019-02-10 15:39:49 -0700

    project edit

---
+++ projects/RPG/myrpg.js
@@ -0,0 +1,91 @@
+var readline = require('readline-sync')
+
+var villains = ["", "Fort", "Blazer"]
+
+var isAlive = true;
+
+var utilityBelt = []
+
+class Sword {
+    constructor
+    (){
+        this.attack = 10;
+        this.defense = 10;
+    }
+
+    attackEnemy(enemy){
+        enemy.hp -= this.attack
+
+    }
+}
+
+
+function superHero (name){
+    this.name = name;
+    this.attack = Math.floor(Math.random()*20);
+    this.defense = 20
+    this.superpower = Math.floor(Math.random()*10)
+}
+
+function
+
+function sideKick(){
+
+}
+
+
+
+
+// Functions :
+
+// Enemy function
+function encounter (){
+    var action = readline.keyIn("Would you like to [e] Escape or [f] Fight? ", {limit: 'ef'})
+    if(action === 'r'){
+        run()
+    }else {
+        var enemy = enemySelect()
+        while(enemy.hp > 0 && player.hp > 0){
+            fight(enemy)
+        }
+        if(enemy.hp <= 0){
+            console.log('you defeated the enemy')
+        }else if(player.hp <= 0){
+            console.log("Wasted.")
+            isAlive = false;
+        }
+    }
+}
+
+// Encounter 
+
+function enemySelect (){
+    var random = Math.floor(Math.random()*enemies.length)
+
+    return enemies[random]
+}
+
+function fight (enemy){
+    var random = Math.floor(Math.random()*5)
+    var missChance
+    if(superHero.defense < 30){
+        missChance = 2.5
+    } else if(superHero.defense < 30 && superHero.defense < 61){
+        missChance = 3
+    } else {
+        missChance = 4
+    }
+    if(random < missChance){
+        miss()
+
+    }else {
+        player.hp -= enemy.attack
+        enemy.hp -= player.attack * superWeapon
+        console.log(`Hey ${player.name} your hp is now ${player.hp}`)
+        console.log(`You hit ${enemy.name} and their hp is ${enemy.hp}`)
+    }
+}
+
+function miss (){
+    console.log("you missed!")
+}
---
+++ projects/RPG/rpg.js
@@ -0,0 +1,90 @@
+var readline = require('readline-sync')
+
+var isAlive = true;
+var hasWon = false;
+var superWeapon = Hero.attack * 2
+
+// Character Creation
+
+function Hero (name){
+    this.name = name;
+    this.attack = Math.floor(Math.random()*35);
+    this.hp = 100;
+    this.items = [];
+    this.isAlive = true;
+}
+
+var player = new Hero('Big Newt Dawg Man Bro Dude')
+
+function Enemy (name, attack, hp){
+    this.name = name;
+    this.attack = attack;
+    this.hp = hp;
+}
+var randomLow = Math.floor(Math.random()*10)+10;
+var randomMedium = Math.floor(Math.random()*15)+20;
+
+var lexLuthor = new Enemy('Bald', randomLow, 15);
+var bungalowBoy = new Enemy('Bouncy', randomMedium, 50);
+
+var enemies = [lexLuthor, bungalowBoy]
+
+// intro console.logs
+console.log('hello, it is a game')
+
+while(isAlive && !hasWon){
+    var action = readline.keyIn("What would you like to do? [w] Walk, [p] Print Inventory, [q] Quit Game ", {limit: 'wpq'})
+    if(action === 'w'){
+        walk()
+    }
+}
+
+
+function walk (){
+    var random = Math.floor(Math.random()*3)
+    if (random < 1){
+        encounter()
+    }else {
+        console.log('You passed a pretty tree')
+    }
+}
+
+function encounter (){
+    var action = readline.keyIn("Would you like to [r] Run or [f] Fight? ", {limit: 'rf'})
+    if(action === 'r'){
+        run()
+    }else {
+        var enemy = enemySelect()
+        while(enemy.hp > 0 && player.hp > 0){
+            fight(enemy)
+        }
+        if(enemy.hp <= 0){
+            console.log('good job you killed the enemy')
+        }else if(player.hp <= 0){
+            console.log('You dead')
+            isAlive = false;
+        }
+    }
+}
+
+// Encounter Function
+function fight (enemy){
+    var random = Math.floor(Math.random()*5)
+    if(random < 2.5){
+        miss()
+    }else {
+        player.hp -= enemy.attack
+        enemy.hp -= player.attack * superWeapon
+        console.log(`Hey ${player.name} your hp is now ${player.hp}`)
+        console.log(`You hit ${enemy.name} and their hp is ${enemy.hp}`)
+    }
+}
+// Random Enemy Function
+function enemySelect (){
+    var random = Math.floor(Math.random()*enemies.length)
+    return enemies[random]
+}
+
+function miss (){
+    console.log('you missed dawg')
+}
