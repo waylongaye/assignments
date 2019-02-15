fedcdc96b939c2e309fff8bc1c8641a494a396ba
Waylon Gaye <waylongaye@gmail.com>
2019-02-15 12:02:11 -0700

    axios & zen garden, notes

--- projects/axios/part1/axiosNotes.js
+++ projects/axios/part1/axiosNotes.js
@@ -1,11 +1,13 @@
 axios.get("https://api.vschool.io/waylon/todo").then(response => {
-    renderTodos(response.data)
+    response.data.forEach(function(todo){
+    displayTodo(todo)
+    })
 })
 
-function renderTodos(todos){ /// this represents renderTodos(response.data) ABOVE
+function displayTodo(todo){ /// this represents displayTodos(response.data) ABOVE
 
-    todos.forEach(function(todo){ // todos.forEach represents the array of sent data named response.data
-        var parent = createParent() // parent is now the parent div
+    // todos.forEach(function(todo){ // todos.forEach represents the array of sent data named response.data
+        // var parent = createParent() // parent is now the parent div
         var parent = document.createElement("div"); // the div tag is now the parent div
         parent.className = "todo"; // assigns the "todo" class name to the parent div
         var text = document.createTextNode(todo.title); // .createTextNode(todo.title; this is response.data.title) creates a text string out of the value (todo.title)
@@ -23,14 +25,29 @@ function renderTodos(todos){ /// this represents renderTodos(response.data) ABOV
         
         if(todo.completed){
             parent.classList.toggle("strikened");
-            input.checked = true;
+            input.checked = true; // .checked sets or retrieves the state of the checkbox
         }
 
         document.getElementById("todo-list").appendChild(parent);
-    })
+    
 }
 
 function handleChecked(e){
     console.log("here");
     e.target.parentNode.classList.toggle("strikened");
 }
+
+document.form.addEventListener("submit", function(e){
+    e.preventDefault(); // e.preventDefault prevents refresh on the submit event
+
+    newObject= {
+        title: document.form.title.value,
+        description: document.form.description.value,
+        price: document.form.price.value,
+        imgUrl: document.form.image.value
+    } // within the "post" you create your new obe
+})
+axios.post("https://api.vschool.io/waylon/todo", newObject).then(response =>{
+    console.log(response.data);
+    displayTodo(response.data);
+})
---
+++ projects/axios/part1/myget.js
@@ -0,0 +1,7 @@
+axios.get("https://api.vschool.io/waylon/todo").then(response => { renderTodos(res.data)
+
+})
+
+function renderTodos(todoList){
+    
+}
