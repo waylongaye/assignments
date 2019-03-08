c470d52e6584414b8f7c9a83a28ad8ce94595014
Waylon Gaye <waylongaye@gmail.com>
2019-03-08 09:29:57 -0700

    personalAPI

---
+++ projects/personalAPI/my-app/src/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Home from "./Home.js"
+import About from "./About.js"
+import Inspire from "./Inspire.js"
+
+
+
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <Home />
+                <About />
+                <Inspire />
+            </div>
+        );
+    }
+}
+
+export default componentName;
